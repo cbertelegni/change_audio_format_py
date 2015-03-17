460821064e240d71262521b6ad9516f1be492504
cristian Bertelegni <cristianbertelegni@gmail.com>
2015-03-17 16:53:58 -0300

    saves all files

--- mp3_to_wavogg.py
+++ mp3_to_wavogg.py
@@ -2,25 +2,30 @@
 # -*- coding: utf-8 -*-
 
 from pydub import AudioSegment
-import os, glob
+import os, glob, sys
 
 
-mp3_path = 'path_to/mp3'
+root_mp3_path = 'indec_mp3'
 root_output = 'audios'
 wav_path = os.path.join(root_output, 'wav')
 ogg_path = os.path.join(root_output, 'ogg')
+mp3_path = os.path.join(root_output, 'mp3')
 
 
-def main():
-	for mp3 in glob.glob(os.path.join(mp3_path,'*.mp3') ):
+def main(folder = root_mp3_path, files_type ='*.mp3'):
+	for mp3 in glob.glob(os.path.join(folder, files_type) ):
 	# for mp3 in mp3s:
 		""" convert mp3 to wav """
 		wav_name =  mp3.split('/')[-1].replace('.mp3', '.wav')
 		ogg_name =  mp3.split('/')[-1].replace('.mp3', '.ogg')
-		src_wav = os.path.join(wav_path, wav_name)
-		src_ogg = os.path.join(ogg_path, ogg_name)
+		mp3_name = mp3.split('/')[-1]
+	
+		src_mp3 = os.path.join(mp3_path, mp3_name).replace(' ', '_').lower()
+		src_wav = os.path.join(wav_path, wav_name).replace(' ', '_').lower()
+		src_ogg = os.path.join(ogg_path, ogg_name).replace(' ', '_').lower()
 		
-		sound = AudioSegment.from_mp3(mp3)
+		sound = AudioSegment.from_file(mp3)
+		sound.export(src_mp3, format="mp3")
 		sound.export(src_ogg, format="ogg")
 		sound = sound.set_channels(1) # set to mono
 		sound.export(src_wav, format="wav")
@@ -31,12 +36,22 @@ if __name__ == "__main__":
 	    """ Check if OUTPUT_DIR exits """
 	    os.mkdir(root_output)
 
+	if not os.path.isdir(mp3_path):
+	    """ Check if OUTPUT_DIR exits """
+	    os.mkdir(mp3_path)
+	
 	if not os.path.isdir(wav_path):
 	    """ Check if OUTPUT_DIR exits """
 	    os.mkdir(wav_path)
-	
+
 	if not os.path.isdir(ogg_path):
 	    """ Check if OUTPUT_DIR exits """
 	    os.mkdir(ogg_path)
-	
-	main()
+
+	try:
+		main(folder=sys.argv[1])
+	except:
+		print 'error'
+		main()
+
+
